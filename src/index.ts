f42e2667db3221947204f7fd8fe373304a9ceb19
Chauhan Aayush <aayushb9157@gmail.com>
2024-07-11 00:24:07 +0530

    [Change] Minor Changes in Index: Added Route File and Comments

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import dbConnect from './config/db';
+import authRoutes from './routes/authRoutes';
+import todoRoutes from './routes/todoRoutes';
 
 const app = express();
 
 // Load environment variables
 dotenv.config();
 
+//For Connection with DB 
 dbConnect();
 
+//Middleware for parses incoming requests with JSON payloads
 app.use(bodyParser.json());
+
+//Define API Routes
+app.use('/api', authRoutes);
+app.use('/api', todoRoutes);
 
 
 const PORT = process.env.PORT || 3000;
