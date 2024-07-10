29d9a6d2b5cb86db271cbd68a1cd98d5d7e6d26c
Chauhan Aayush <aayushb9157@gmail.com>
2024-07-11 00:25:06 +0530

    [change] Added Comments

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 
-
-const options: { useNewUrlParser: true; serverSelectionTimeoutMS: number;socketTimeoutMS: number; connectTimeoutMS: number }= {
-    useNewUrlParser: true,
-    serverSelectionTimeoutMS: 30000,
-    socketTimeoutMS: 45000,      
-    connectTimeoutMS: 30000,     
+const options: { useNewUrlParser: boolean; serverSelectionTimeoutMS: number;socketTimeoutMS: number; connectTimeoutMS: number }= {
+    useNewUrlParser: true, // use the new MongoDB connection string parser
+    serverSelectionTimeoutMS: 30000, // If the server is not selected within 30 seconds, the connection attempt will fail
+    socketTimeoutMS: 45000, //  If there's no activity on the socket for 45 seconds, the connection will be closed.   
+    connectTimeoutMS: 30000,  // If the connection is not established within 30 seconds, the attempt will fail.
   };
 const dbConnect = async () => {
   try {
