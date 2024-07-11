4ac4c444a3a05cbba3c37e905ba1f9363eefc11a
Chauhan Aayush <aayushb9157@gmail.com>
2024-07-11 23:15:41 +0530

    [add] Authentication Middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,27 +1,32 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import User, { IUser } from '../models/userModel';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User, { IUser } from "../models/userModel";
+import { AppError } from "./errorHandler";
 
 interface AuthRequest extends Request {
   user?: IUser;
 }
 
-export const authenticate = async (req: AuthRequest, res: Response, next: NextFunction) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+export const authenticate = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
   // If there is no any token provided in header
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
+    return next(new AppError("No token provided", 401));
   }
 
   try {
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
     const user = await User.findById(decoded.id);
     if (!user) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return next(new AppError("Invalid token", 401));
     }
     req.user = user;
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Invalid token' });
+    next(new AppError("Invalid token", 401));
   }
 };
