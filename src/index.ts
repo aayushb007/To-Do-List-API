d0db28f57f837d7bdfe4c35b274a7a7d5731d05f
Chauhan Aayush <aayushb9157@gmail.com>
2024-07-11 23:15:02 +0530

    [Add] Middlewares and required comments

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import dbConnect from './config/db';
-import authRoutes from './routes/authRoutes';
-import todoRoutes from './routes/todoRoutes';
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import dbConnect from "./config/db";
+import authRoutes from "./routes/authRoutes";
+import todoRoutes from "./routes/todoRoutes";
+import logger from "./middlewares/loggerMiddleware";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
-// Load environment variables
-dotenv.config();
+dotenv.config(); // Load environment variables
 
-//For Connection with DB 
-dbConnect();
+dbConnect(); //For Connection with DB
 
-//Middleware for parses incoming requests with JSON payloads
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //Middleware for parses incoming requests with JSON payloads
+
+app.use(logger); //Middleware for request logging
 
 //Define API Routes
-app.use('/api', authRoutes);
-app.use('/api', todoRoutes);
+app.use("/api", authRoutes);
+app.use("/api", todoRoutes);
 
+app.use(errorHandler); //Error Handler Middleware
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+//During testing, the server doesn't start automatically.
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+export default app;
